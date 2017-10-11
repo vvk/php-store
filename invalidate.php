2938ad887e4d681cab50cdfe1dd5ef55bc142574
Vladislav Kaverin <vladislav.v.kaverin@gmail.com>
2017-10-11 23:48:43 +0400

    Log total cache invalidation (for dev).

--- invalidate.php
+++ invalidate.php
@@ -2,5 +2,9 @@
 
 include 'cache.php';
 
-get_cache()->flush();
+if (get_cache()->flush()) {
+    error_log("Cache has been invalidated.");
+} else {
+    error_log("Failed to invalidate cache.");
+}
 header('Location: items.php');
