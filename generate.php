15d84366bcab0d3ac3f934c1095862af2593be79
Vladislav Kaverin <vladislav.v.kaverin@gmail.com>
2017-10-16 00:29:06 +0400

    Generate items in batches.
    Generate items as 10000 per batch to avoid very large transactions.
    Validate input more properly.

--- generate.php
+++ generate.php
@@ -3,31 +3,47 @@
 include_once 'shared.php';
 include_once 'cache.php';
 
-$limit = empty($_GET['limit']) ? 1 : $_GET['limit'];
+const MAX_BATCH_SIZE = 10000;
+
+$limit = !array_key_exists('limit', $_GET) ? 1 : $_GET['limit'];
+
+if (!is_numeric($limit) || $limit < 1) {
+    die("Huh, very funny. Invalid items quantity or format specified.");
+}
+
+if ($limit > 1000 * 1000) {
+    die("Wow-wow, hold on: $limit is too much. One million items per request is the maximum.");
+}
 
 $mysqli = get_client();
 $mysqli->autocommit(false);
-$mysqli->begin_transaction();
 $stmt = $mysqli->prepare('INSERT INTO items(name, description, price) VALUES (?, ?, ?)');
 
 $lorem = file_get_contents('lorem.txt');
 $paragraphs = explode(PHP_EOL, $lorem);
 $total_paragraphs = count($paragraphs);
 
-for ($i = 0; $i < $limit; ++$i) {
-    $name = 'Item '.uniqid('id:');
-    $description = $paragraphs[rand(0, $total_paragraphs - 1)];
-    $price = time();
+while ($limit > 0) {
+    $batch_size = min($limit, MAX_BATCH_SIZE);
+    $mysqli->begin_transaction();
+    for ($i = 0; $i < $batch_size; ++$i) {
+        $name = 'Item '.uniqid('id:');
+        $description = $paragraphs[rand(0, $total_paragraphs - 1)];
+        $price = time();
 
-    $stmt->bind_param('ssd', $name, $description, $price);
-    if (!$stmt->execute()) {
-        die("Fuck this shit: ".$mysqli->error);
+        $stmt->bind_param('ssd', $name, $description, $price);
+        if (!$stmt->execute()) {
+            die("Fuck this shit: ".$mysqli->error);
+        }
     }
+
+    if ($mysqli->commit()) {
+        echo "Generated batch of $batch_size new items. <br />";
+    } else {
+        die("Could not commit transaction.");
+    }
+    $limit -= $batch_size;
 }
 
-if ($mysqli->commit()) {
-    echo "Generated $limit new items.";
-    invalidate_cache();
-} else {
-    die("Could not commit transaction.");
-}
+_free_resources($stmt, $mysqli);
+invalidate_cache();
